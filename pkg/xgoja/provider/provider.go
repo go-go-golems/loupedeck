2c43f39bfaf9bcb610725bbd3c8d455288158b5c
Manuel Odendahl <wesen@ruinwesen.com>
2026-05-24 23:21:00 -0400

    test: add xgoja smoke for loupedeck provider

---
+++ pkg/xgoja/provider/provider.go
@@ -0,0 +1,12 @@
+package provider
+
+import (
+	"github.com/go-go-golems/go-go-goja/pkg/xgoja/providerapi"
+	jsprovider "github.com/go-go-golems/loupedeck/runtime/js/provider"
+)
+
+const PackageID = jsprovider.PackageID
+
+func Register(registry *providerapi.Registry) error {
+	return jsprovider.Register(registry)
+}
